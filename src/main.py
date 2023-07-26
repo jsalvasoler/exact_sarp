ffc66487b7c9628e2d04b5a16fbe39d6930ef55d
Joan Salvà Soler <jsalvasoler@hotmail.com>
2023-07-26 19:03:20 +0200

    Refactor main formulation dictionary

--- src/main.py
+++ src/main.py
@@ -3,30 +3,21 @@ from src.formulations.cutset_formulation import CutSetFormulation
 from src.formulations.scf_formulation import SCFFormulation
 from optimizer import Optimizer
 from src.config import Config
-from utils import Instance
 from src.instance_loader import InstanceLoader
 from pyinstrument import Profiler
 
-
-def test_single_instance():
-    instance = Instance(75, 15, 50, 150, seed=0)
-    config = Config()
-    formulations = {
-        'mtz': MTZFormulation(instance, config.activations.get('mtz', {})),
-    }
-    optimizer = Optimizer(formulations['mtz'], config)
-    optimizer.run()
+formulations = {
+    'mtz': MTZFormulation,
+    'cutset': CutSetFormulation,
+    'scf': SCFFormulation,
+}
 
 
 def main():
     config = Config()
     instance_loader = InstanceLoader(config)
     instances = instance_loader.load_instances()
-    formulations = {
-        'mtz': MTZFormulation,
-        'cutset': CutSetFormulation,
-        'scf': SCFFormulation,
-    }
+
     for i, (name, instance) in enumerate(instances.items()):
         print(f'\nInstance {i + 1}/{len(instances)}: \n  id = {name[:2]}, name = {name[3:-4]}\n\n')
         instance.print()
@@ -40,7 +31,6 @@ if __name__ == '__main__':
 
     profiler = Profiler()
     profiler.start()
-    # test_single_instance()
     main()
     profiler.stop()
     print(profiler.output_text(unicode=True, color=True))
