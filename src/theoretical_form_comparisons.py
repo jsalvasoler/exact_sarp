45a375df3f27bb4b29889297cf55b6ebed2adaca
Joan Salvà Soler <jsalvasoler@hotmail.com>
2023-08-07 16:20:37 +0200

    Develop theory

--- src/theoretical_form_comparisons.py
+++ src/theoretical_form_comparisons.py
@@ -8,6 +8,15 @@ import gurobipy as gp
 
 
 def run_experiment(draw, seed=1):
+    """
+    This experiment checks whether it is true or not the P_SCF \subset P_MTZOPT by doing the following:
+    1. Generate a random instance
+    2. Solve the LR of the SCF formulation
+    3. Build a solution for the MTZOPT formulation using the solution of the LR of the SCF formulation. Define the
+    u_i values based on the f_ij values of the SCF formulation.
+    4. Check whether the solution is feasible for the LR of the MTZOPT formulation
+    """
+
     instance = Instance(10, 3, 8, 1, seed=seed)
     scf_form = SCFFormulation(instance, linear_relax=True)
     scf_form.formulate()
@@ -18,7 +27,7 @@ def run_experiment(draw, seed=1):
 
     if scf_form.solver.status == gp.GRB.INFEASIBLE:
         warnings.warn('SCF formulation is infeasible')
-        return
+        return True
 
     for k, v in scf_form.y.items():
         if v.X > 0:
@@ -43,33 +52,61 @@ def run_experiment(draw, seed=1):
         print(f'u_{k}: {v}')
 
     if draw:
-        # Draw the K5 graph with x, f values on the edges and y, u values on the nodes
-        import networkx as nx
-        import matplotlib.pyplot as plt
-        G = nx.DiGraph()
-        G.add_nodes_from(instance.N_0)
-        G.add_edges_from([(i, j) for i in instance.N_0 for j in instance.N_0 if i != j and x[i, j] > 0])
-        pos = nx.circular_layout(G)
-        nx.draw_networkx(G, pos=pos, with_labels=True)
-        node_labels = {i: f'y_{i}={y[i]}\nu_{i}={u[i]}' for i in instance.N if y[i] > 0}
-        nx.draw_networkx_labels(G, pos=pos, labels=node_labels)
-        edge_labels = {(i, j): f'x_{i, j}={x[i, j]}\nf_{i, j}={x[i, j]}\nt_{i, j}={instance.t[i, j]}' for i in instance.N_0
-                       for j in instance.N_0 if i != j and x[i, j] > 0}
-        nx.draw_networkx_edge_labels(G, pos=pos, edge_labels=edge_labels)
-        plt.show()
+        draw_solution(instance, u, x, y)
 
-    print(f'Objective: {scf_form.solver.objVal}')
-    mtz_form = MTZOptFormulation(instance, linear_relax=True)
-    mtz_form.formulate()
-    mtz_form.impose_solution(x, y, u)
-    mtz_form.solver.setObjective(gp.quicksum(profits[i] * mtz_form.y[i] for i in instance.N), gp.GRB.MAXIMIZE)
-    mtz_form.solver.optimize()
+    # Check whether the solution is feasible for the MTZOPT formulation
+    satisfied_mtz = {}
+    for i in instance.N:
+        for j in instance.N_0:
+            if i == j:
+                continue
+            lhs = round(u[j] - u[i], 4)
+            rhs = round(x[i, j] * (instance.T_max + instance.t[i, j]) - instance.T_max, 4)
+            satisfied_mtz[i, j] = True if lhs >= rhs else False
+            print(f'MTZ for {i}->{j}\n   {lhs} >= {rhs}: {True if satisfied_mtz[i, j] else False}')
+    satisfied_extra = {}
+    for i in instance.N:
+        print(f'Extra constraints for {i}')
+        satisfied_extra[i] = 0 <= u[i] <= instance.T_max
+        print(f'   {0} <= {u[i]} <= {instance.T_max}: {True if satisfied_extra[i] else False}')
 
-    if mtz_form.solver.status == gp.GRB.INFEASIBLE:
-        infeasible_analysis(mtz_form.solver)
-        raise Exception('MTZ formulation is infeasible')
+    print(f'Satisfied MTZ constraints: {sum(satisfied_mtz.values())} / {len(satisfied_mtz)}')
+    print(f'Satisfied extra constraints: {sum(satisfied_extra.values())} / {len(satisfied_extra)}')
 
-    assert abs(mtz_form.solver.ObjVal - scf_form.solver.ObjVal) < 1e-6
+    if sum(satisfied_mtz.values()) != len(satisfied_mtz) or sum(satisfied_extra.values()) != len(satisfied_extra):
+        return False
+    return True
+
+    # print(f'Objective: {scf_form.solver.objVal}')
+    # mtz_form = MTZOptFormulation(instance, linear_relax=True)
+    # mtz_form.formulate()
+    # mtz_form.impose_solution(x, y, u)
+    # mtz_form.solver.setObjective(gp.quicksum(profits[i] * mtz_form.y[i] for i in instance.N), gp.GRB.MAXIMIZE)
+    # mtz_form.solver.optimize()
+    #
+    # if mtz_form.solver.status == gp.GRB.INFEASIBLE:
+    #     infeasible_analysis(mtz_form.solver)
+    #     raise Exception('MTZ formulation is infeasible')
+    #
+    # assert abs(mtz_form.solver.ObjVal - scf_form.solver.ObjVal) < 1e-6
+
+
+def draw_solution(instance, u, x, y):
+    # Draw the graph with x, f values on the edges and y, u values on the nodes
+    import networkx as nx
+    import matplotlib.pyplot as plt
+    G = nx.DiGraph()
+    G.add_nodes_from(instance.N_0)
+    G.add_edges_from([(i, j) for i in instance.N_0 for j in instance.N_0 if i != j and x[i, j] > 0])
+    pos = nx.circular_layout(G)
+    nx.draw_networkx(G, pos=pos, with_labels=True)
+    node_labels = {i: f'y_{i}={y[i]}\nu_{i}={u[i]}' for i in instance.N if y[i] > 0}
+    nx.draw_networkx_labels(G, pos=pos, labels=node_labels)
+    edge_labels = {(i, j): f'x_{i, j}={x[i, j]}\nf_{i, j}={x[i, j]}\nt_{i, j}={instance.t[i, j]}' for i in
+                   instance.N_0
+                   for j in instance.N_0 if i != j and x[i, j] > 0}
+    nx.draw_networkx_edge_labels(G, pos=pos, edge_labels=edge_labels)
+    plt.show()
 
 
 def infeasible_analysis(solver):
@@ -86,6 +123,10 @@ def infeasible_analysis(solver):
 
 
 if __name__ == '__main__':
-    for seed in range(1, 1000):
+    for seed in range(1, 100):
         print(f'Seed: {seed}')
-        run_experiment(draw=False, seed=seed)
+        if not run_experiment(draw=False, seed=seed):
+            print('False!')
+            break
+
+    print('Done')
